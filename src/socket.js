7d48e099e9bbdfe64e0adcd16d0aac102c6ce5e7
Sahith Akula <akulasahith268@gmail.com>
2026-01-13 00:02:17 +0530

    First Push

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://rd-timer-backend.onrender.com");
-
-export default socket;
+const socket = io("https://rd-timer-backend.onrender.com", {
+  transports: ["websocket"],
+});
